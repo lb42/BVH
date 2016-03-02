b6ae2510c292dafbdc74f474d8015512e9468fef
Frédéric Glorieux <frederic.glorieux@fictif.org>
2016-03-02 18:17:15 +0000

    Molière graphes V2

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (strpos($path, 'moliere') === 0) {
   $play = $pdomol->query("SELECT * FROM play WHERE code = ".$pdomol->quote($playcode))->fetch();
   $moliere = true;
 }
-// ou alors on charge de la critique sous critique/…, 
+// ou alors on charge de la critique sous critique/…,
 else if (strpos($path, 'critique/') === 0) {
   // noter, le path relatif dans la base critique.sqlite
   $pot = new Teipot(dirname(__FILE__).'/critique-moliere.sqlite', 'fr', substr($path, 9));
@@ -60,74 +60,55 @@ $theme = Web::basehref().'../theme/'; // autres ressources spécifiques
     <meta charset="UTF-8" />
     <link rel="stylesheet" charset="utf-8" type="text/css" href="<?php echo $teinte; ?>tei2html.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo $theme; ?>obvil.css" />
-    <style>
-/* couleur des rôles par genre */
-.charline .role { background-color: rgba(192, 192, 192, 0.7); color: rgba(0, 0, 0, 0.5); }
-.charline .female { background-color: rgba(255, 0, 0, 0.5); color: rgba(255, 255, 255, 1);}
-.charline .female.junior { background-color: rgba(255, 64, 128, 0.3); color: rgba(0, 0, 0, 0.7);}
-.charline .female.inferior { background-color: rgba(192, 96, 128, 0.4); color: rgba(255, 255, 255, 1);}
-.charline .female.veteran { background-color: rgba(128, 0, 0, 0.4); color: rgba(255, 255, 255, 1);}
-.charline .male { background-color: rgba(0, 0, 255, 0.4); color:  rgba(255, 255, 255, 1);}
-.charline .male.junior { background-color: rgba(0, 192, 255, 0.2); color: rgba(0, 0, 0, 0.7);}
-.charline .male.inferior { background-color: rgba(96, 96, 192, 0.3); color: rgba(255, 255, 255, 1);}
-.charline .male.veteran { background-color: rgba(0, 0, 128, 0.4); color:  rgba(255, 255, 255, 1);}
-.charline .male.superior { background-color: rgba(0, 0, 255, 0.6); color:  rgba(255, 255, 255, 1);}
-
-    </style>
-    <?php 
-if (isset($moliere)) {
-  echo '
+    <link rel="stylesheet" charset="utf-8" type="text/css" href="<?php echo Web::basehref().'../'; ?>Dramaturgie/dramaturgie.css"/>
+    <script src="<?php echo Web::basehref().'../'; ?>Dramaturgie/sigma/sigma.min.js">//</script>
+    <script src="<?php echo Web::basehref().'../'; ?>Dramaturgie/sigma/sigma.layout.forceAtlas2.min.js">//</script>
+    <script src="<?php echo Web::basehref().'../'; ?>Dramaturgie/sigma/sigma.plugins.dragNodes.min.js">//</script>
+    <script src="<?php echo Web::basehref().'../'; ?>Dramaturgie/sigma/sigma.exporters.image.min.js">//</script>
+    <script src="<?php echo Web::basehref().'../'; ?>Dramaturgie/Rolenet.js">//</script>
     <style>
 #article { padding: 0; }
-#aside { padding-top: 3px; } 
     </style>
-    <script src="sigma/sigma.min.js">//</script>
-    <script src="sigma/sigma.layout.forceAtlas2.min.js">//</script>
-    <script src="sigma/sigma.plugins.dragNodes.min.js">//</script>
-    <script src="Rolenet.js">//</script>
-  ';
-}
-if(isset($doc['head'])) echo $doc['head']; 
+    <?php
+if(isset($doc['head'])) echo $doc['head'];
 else echo '
 <title>OBVIL, Molière</title>
 ';
     ?>
-    <style type="text/css">
-div.snip a.bookmark { display: none; }
-    </style>
   </head>
   <body>
     <div id="center">
       <header id="header">
         <h1>
-          <a href="<?php echo Web::$basehref; ?>">OBVIL, Molière</a>
+          <a href="<?php
+
+if (isset($play)) echo "moliere";
+else echo Web::$basehref;
+          ?>">OBVIL, Molière</a>
         </h1>
         <a class="logo" href="http://obvil.paris-sorbonne.fr/"><img class="logo" src="<?php echo $theme; ?>img/logo-obvil.png" alt="OBVIL"></a>
-        <?php 
-         
-if (isset($moliere)) { // navigation par select
-  echo ' 
-<form name="net" style="position: fixed; z-index: 3; top: 2px; left: 492px; " action="#">
-  <select name="play" onchange="this.form.action = this.options[this.selectedIndex].value+\'#graph\'; this.form.submit()">'."\n";
-  echo "    <option>  </option>\n";
-  foreach ($pdomol->query("SELECT * FROM play ORDER BY author, year") as $row) {
-    if ($row['code'] == $playcode) $selected=' selected="selected"';
-    else $selected = "";
-    echo '    <option value="'.$row['code'].'"'.$selected.'>'.bibl($row)."</option>\n";
-  }
-  echo '  </select>
-  <a href="#" class="but">▲</a>
-</form>';
-}
-        ?>
       </header>
       <div id="contenu">
         <div id="main">
           <nav id="toolbar">
             <nav class="breadcrumb">
-            <?php 
+            <?php
 if (isset($critique)) echo '<a href="' . Web::basehref() . 'critique/' . $pot->qsa(null, null, '?') . '">Critique moliéresque</a> &gt; ';
-if (isset($doc['breadcrumb'])) echo $doc['breadcrumb']; 
+if (isset($doc['breadcrumb'])) echo $doc['breadcrumb'];
+if (isset($play) && $play) { // navigation par select
+  echo '
+<form name="net" action="#">
+  <select name="play" onchange="this.form.action = this.options[this.selectedIndex].value; this.form.submit()">'."\n";
+  echo "    <option>  </option>\n";
+  foreach ($pdomol->query("SELECT * FROM play ORDER BY author, year") as $row) {
+    if ($row['code'] == $playcode) $selected=' selected="selected"';
+    else $selected = "";
+    if ($row['year']) $row['year'] .= ', ';
+    echo '    <option value="'.$row['code'].'"'.$selected.'>'.$row['year'].$row['title']."</option>\n";
+  }
+  echo '  </select>
+</form>';
+}
             ?>
             </nav>
           </nav>
@@ -135,30 +116,49 @@ if (isset($doc['breadcrumb'])) echo $doc['breadcrumb'];
       <?php
 if (isset($moliere) && !$play) {
   echo '
-<table class="sortable">
-  <tr><th>Date</th><th>Titre</th></tr>
-';
-  foreach ($pdomol->query("SELECT * FROM play ORDER BY author, year") as $play) {
-    echo '<tr><td>'.$play['year'].'</td><td><a href="'.$play['code'].'">'.$play['title']."</a></td></tr>\n";
+  <table class="sortable">
+    <tr>
+      <th>Date</th>
+      <th>Titre</th>
+      <th>Paroles</th>
+      <th>Répliques</th>
+      <th>Rép. moy.</th>
+      <th>Personnages</th>
+      <th>Prés. moy.</th>
+    </tr>';
+  ;
+  foreach ($pdomol->query("
+    SELECT
+      *,
+      (SELECT COUNT(*) FROM role WHERE play = play.id) AS roles,
+      (SELECT SUM(roles * c) FROM configuration WHERE play = play.id) AS pres
+    FROM play ORDER BY author, year") as $row) {
+    echo '<tr>'."\n";
+    echo '<td>'.$row['year'].'</td>'."\n";
+    echo '<td>'.'<a href="'.$row['code'].'">'.$row['title']."</a></td>\n";
+    echo '<td align="right">'.number_format($row['c']/60, 0).' l.</td>';
+    echo '<td align="right">'.$row['sp'].'</td>';
+    echo '<td align="right">'.number_format($row['c']/$row['sp']/60, 2).' l.</td>';
+    echo '<td align="right">'.$row['roles'].'</td>';
+    echo '<td align="right">'.number_format($row['pres']/$row['c'], 1).' pers.</td>';
+    echo '</tr>'."\n";
+
   }
-  echo "\n</table>\n";
+  echo '</table>'."\n";
 }
 else if (isset($moliere)) {
-  echo '
-        <div id="graph" style="height: 500px; position: relative; background: #FFFFFF; ">
-
-          <div style="position: absolute; bottom: 0; right: 0; z-index: 2; ">
-            <button class="mix but" type="button" title="Mélanger le graphe">♻</button>
-            <button class="grav but" type="button" title="Démarrer ou arrêter la gravité">►</button>
-          </div>
-        </div>
-      <script>
-var data =';  
-$qobj->execute(array($playcode, 'sigma'));
-$res = $qobj->fetch(PDO::FETCH_ASSOC);
-echo $res['cont'];
-echo ' var graph1 = new Rolenet("graph", data, "sigma/worker.js"); // 
-      </script>';
+  $qobj->execute(array($playcode, 'canvas'));
+  $row = $qobj->fetch(PDO::FETCH_ASSOC);
+  echo $row['cont'];
+  echo'<script> var data =';
+  $qobj->execute(array($playcode, 'sigma'));
+  $row = $qobj->fetch(PDO::FETCH_ASSOC);
+  echo $row['cont'];
+  echo ' var graph = new Rolenet("graph", data, "../Dramaturgie/sigma/worker.js"); //';
+  echo "    </script>\n";
+  $qobj->execute(array($playcode, 'nodetable'));
+  $row = $qobj->fetch(PDO::FETCH_ASSOC);
+  echo $row['cont'];
   $qobj->execute(array($playcode, 'article'));
   $res = $qobj->fetch(PDO::FETCH_ASSOC);
   echo '<div style="padding: 0 20px 50px 45px; ">'.$res['cont'].'</div>';
@@ -187,8 +187,7 @@ else if($pot) {
           <?php
 // pièce de Théâtre
 if(isset($moliere) && $play) {
-
-  echo "\n".'<nav id="download"><small>Télécharger :</small> 
+  echo "\n".'<nav id="download"><small>Télécharger :</small>
   <a href="http://dramacode.github.io/epub/'.$playcode.'.epub">epub</a>,
   <a href="http://dramacode.github.io/kindle/'.$playcode.'.mobi">kindle</a>,
   <a href="http://dramacode.github.io/iramuteq/'.$playcode.'.txt">iramuteq</a>,
@@ -224,7 +223,7 @@ else if (isset($doc['bookrowid'])) {
   echo "\n</nav>";
 }
 // accueil ? formulaire de recherche général
-else if(isset($moliere) || isset($critique)) {
+else if(isset($critique)) {
   echo'
     <form action="" style="text-align:center">
       <input name="q" id="q" class="search" size="20" onclick="this.select()" placeholder="Rechercher" class="text" value="'.str_replace('"', '&quot;', $q).'"/>
@@ -245,10 +244,6 @@ else if(isset($moliere) || isset($critique)) {
  * Ligne bibliographique pour une pièce
  */
 function bibl($play) {
-  if (is_string($play)) {
-    $playcode = $this->pdo->quote($playcode);
-    $play = $this->pdo->query("SELECT * FROM play WHERE code = $playcode")->fetch();
-  }
   $bibl = $play['author'].', '.$play['title'];
   $meta = array();
   if ($play['year']) $meta[] = $play['year'];
@@ -259,6 +254,6 @@ function bibl($play) {
   if (count($meta)) $bibl .= " (".implode(", ", $meta).")";
   return $bibl;
 }
-  
+
 $pdomol = null;
 ?>
