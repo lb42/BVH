eed1fb875955908985f7a6c4b787a4a169341ccf
Frédéric Glorieux <frederic.glorieux@fictif.org>
2016-10-31 13:09:53 +0000

    OK sur obvil-dev

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ BASEDIR=$(dirname $0)
 export LC_CTYPE=fr_FR.UTF-8 # pour les noms de fichiers avec accents
 /usr/bin/svn --non-interactive --trust-server-cert  update $BASEDIR/
 /usr/bin/php $BASEDIR/../teipub/Teipub.php  "$BASEDIR/critique-moliere.xml"  "$BASEDIR/critique-moliere.sqlite" 2>&1
+/usr/bin/php $BASEDIR/../teipub/Teipub.php  "$BASEDIR/critique/*.xml"  "$BASEDIR/critique-moliere.sqlite" 2>&1
+
 exit 0
