568424e75af22c25723eabc6e5e2bf0ce4943b7c
benard <benard@884c693f-bd82-4afc-8945-aec71f254af5>
2017-04-12 16:06:06 +0000

    ajout

---
+++ anecdotes/tpl/form.tpl.php
@@ -0,0 +1,21 @@
+<form method="post" >
+<p>Rechercher</p>
+<select name="content type">
+<option value="anecdote">Dans l'anecdote</option>
+<option value="commentaire">Dans le commentaire</option>
+<option value="les-deux">Dans les deux</option
+</select>
+<input type="text" name="content"/>
+<p>Auteur</p>
+<select name="book"></br>
+</select>
+<p>Mots clés</p>
+<select name="keywords"></br>
+</select>
+<p>De</p>
+<input type="text date" placeholder="AAAA" maxlength="4" value="1705"/>
+<p>à</p>
+<input type="text date" placeholder="AAAA" maxlength="4" value="1919"/>
+</select></br>
+<input type="submit" value="Valider" />
+</form>
