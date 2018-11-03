2cb73305777c2e32080219242e5ae3d4b982ee2c
lb42 <lou.burnard@retired.ox.ac.uk>
2018-11-03 17:47:28 +0100

    first draft of sampler

--- anecdotes/graph.php
+++
@@ -1,8 +0,0 @@
-<?php
-//stocker la liste des textes id=>titre
-//stocker la liste des anecdotes
-//stocker la liste des liens
-//pour chaque lien, si occurrence
-//textes qui ajoutent des anecdotes ou qui se contentent de reprendre
-//bio complètes / spécifiques
-?>
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-php ../Dramagraph/Base.php moliere.sqlite urls_moliere.tsv
-php ../Dramagraph/Base.php contexte.sqlite urls_contexte.tsv
-php ../Teinte/Build.php
